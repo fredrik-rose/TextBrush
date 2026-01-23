Use correct image normalization in Image Generator
The normalization used for classification is not what
should be used for diffusion. The diffusion process
expects the images to be in [-1, 1].

textbrush/applications/imageclassifier.py
@@ -10,6 +10,7 @@ import torch
 import torch.utils.data as torchdata
 
 from torch import nn
+from torchvision.transforms import v2
 
 from textbrush.datasets import mnist
 from textbrush.models import vit
@@ -40,7 +41,17 @@ class ImageClassifier(application.Application):
     """
 
     def __init__(self):
-        dataset = mnist.Mnist(train=True)
+        image_transform = v2.Compose(
+            [
+                v2.ToImage(),
+                v2.ToDtype(torch.float32, scale=True),
+                v2.Normalize(mean=(mnist.MEAN,), std=(mnist.STD,)),
+            ]
+        )
+        dataset = mnist.Mnist(
+            transform=image_transform,
+            train=True,
+        )
         channels, height, width = dataset[0][0].shape
         model = vit.ViT(
             num_classes=NUM_CLASSES,
@@ -56,6 +67,7 @@ class ImageClassifier(application.Application):
             attention_dropout=ATTENTION_DROPOUT,
         )
 
+        self._image_transform = image_transform
         self._loss_function = nn.CrossEntropyLoss()
 
         super().__init__(
@@ -72,12 +84,19 @@ class ImageClassifier(application.Application):
         """
         Classify images.
         """
-        data_loader = torchdata.DataLoader(mnist.Mnist(train=False), batch_size=1, shuffle=True)
+        data_loader = torchdata.DataLoader(
+            mnist.Mnist(
+                transform=self._image_transform,
+                train=False,
+            ),
+            batch_size=1,
+            shuffle=True,
+        )
         for i, (image_tensor, true_label) in enumerate(data_loader):
             if i >= num_images:
                 break
             pred_label = self.model.classify(image_tensor[0], device=device)
-            image = mnist.to_image(image_tensor)
+            image = mnist.denormalize(mnist.tensor_to_image(image_tensor))
             plt.imshow(image, cmap="gray")
             plt.title(f"True: {true_label[0]}, Predicted: {pred_label}")
             plt.axis("off")
@@ -107,7 +126,10 @@ class ImageClassifier(application.Application):
         """
         Evaluate the model in the validation dataset.
         """
-        validation_dataset = mnist.Mnist(train=False)
+        validation_dataset = mnist.Mnist(
+            transform=self._image_transform,
+            train=False,
+        )
         data_loader = torchdata.DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True)
         validation_loss = modeltrainer.eval_model(
             model=self.model,

textbrush/applications/imagegenerator.py
@@ -6,10 +6,12 @@ import pathlib
 import typing
 
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.utils.data as torchdata
 
 from torch import nn
+from torchvision.transforms import v2
 
 from textbrush.algorithms import diffusion
 from textbrush.datasets import mnist
@@ -41,12 +43,23 @@ class ImageGenerator(application.Application):
             b_t=NOISE_SCHEDULE_VARIANCE_T,
             time_steps=NOISE_SCHEDULE_STEPS,
         )
+        image_transform = v2.Compose(
+            [
+                v2.ToImage(),  # [0, 255]
+                v2.ToDtype(torch.float32, scale=True),  # [0, 1]
+                v2.Lambda(lambda x: x * 2 - 1),  # [-1, 1]
+            ]
+        )
         dataset = DiffusionDataset(
-            dataset=mnist.Mnist(train=True),
+            dataset=mnist.Mnist(
+                transform=image_transform,
+                train=True,
+            ),
             betas=betas,
         )
 
         self._betas = betas
+        self._image_transform = image_transform
         self._loss_function = nn.MSELoss(reduction="mean")
 
         super().__init__(
@@ -72,7 +85,8 @@ class ImageGenerator(application.Application):
         with LiveImage() as live_image:
             for i, x in enumerate(diffuser.reverse_diffusion(size=size, noise_predictor=self.model)):
                 draw = i % VISUALIZATION_STEPS == 0
-                live_image.update(x, draw=draw)
+                image = diffusion_denormalize(mnist.tensor_to_image(x))
+                live_image.update(image, draw=draw)
                 plt.title(f"{round((i / diffuser.time_steps) * 100)} %")
 
     def train(
@@ -100,7 +114,10 @@ class ImageGenerator(application.Application):
         Evaluate the model in the validation dataset.
         """
         full_validation_dataset = DiffusionDataset(
-            dataset=mnist.Mnist(train=False),
+            dataset=mnist.Mnist(
+                transform=self._image_transform,
+                train=False,
+            ),
             betas=self._betas,
         )
         validation_dataset = torchdata.Subset(full_validation_dataset, [0])  # FIXME: Remove this line.
@@ -159,7 +176,10 @@ class LiveImage:
     Live image context manager.
     """
 
-    def __init__(self, cmap="gray"):
+    def __init__(
+        self,
+        cmap="gray",
+    ):
         self._cmap = cmap
         self._fig = None
         self._ax = None
@@ -181,12 +201,16 @@ class LiveImage:
         plt.ioff()
         plt.show()
 
-    def update(self, x: torch.Tensor | None = None, draw: bool = False) -> None:
+    def update(
+        self,
+        x: np.ndarray | None = None,
+        draw: bool = False,
+    ) -> None:
         """
         Update the image with an image tensor.
         """
         if x is not None:
-            self._x = x.detach().cpu().squeeze().numpy()
+            self._x = x
 
         if not draw:
             return
@@ -200,3 +224,12 @@ class LiveImage:
         self._img.set_clim(vmin=self._x.min(), vmax=self._x.max())
         self._fig.canvas.draw()
         self._fig.canvas.flush_events()
+
+
+def diffusion_denormalize(image: np.ndarray) -> np.ndarray:
+    """
+    De-normalize an image generated via diffusion.
+    """
+    image = (image + 1) / 2
+    image = np.clip(image, 0, 1)
+    return image

textbrush/datasets/mnist.py
@@ -3,13 +3,12 @@ The MNIST (Modified National Institute of Standards and Technology) dataset.
 """
 
 import pathlib
+import typing
 
 import numpy as np
 import torch
 import torch.utils.data as torchdata
 import torchvision
-
-from torchvision.transforms import v2
 
 MEAN = 0.1307
 STD = 0.3081
@@ -24,18 +23,13 @@ class Mnist(torchdata.Dataset):
 
     def __init__(
         self,
+        transform: typing.Callable,
         train: bool,
     ):
         self._dataset = torchvision.datasets.MNIST(
             root=DATASET_PATH,
             train=train,
-            transform=v2.Compose(
-                [
-                    v2.ToImage(),
-                    v2.ToDtype(torch.float32, scale=True),
-                    v2.Normalize(mean=(MEAN,), std=(STD,)),
-                ]
-            ),
+            transform=transform,
             download=True,
         )
 
@@ -49,12 +43,11 @@ class Mnist(torchdata.Dataset):
         return self._dataset[idx]
 
 
-def to_image(tensor: torch.Tensor) -> np.ndarray:
+def tensor_to_image(tensor: torch.Tensor) -> np.ndarray:
     """
     Convert a MNIST image tensor to Numpy image.
     """
-    image = tensor.squeeze().numpy()
-    image = denormalize(image)
+    image = tensor.detach().cpu().squeeze().numpy()
     return image
 
 